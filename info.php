7b4487279e1d13343fb75c5aa126c28f738cfb6f
iNik Dev <mail@inik.pw>
2017-01-13 05:28:00 +0900

    Create info.php

---
+++ info.php
@@ -0,0 +1,13 @@
+<?php
+// Theme Name
+$name = 'Friendify Material';
+
+// Theme Author
+$author = 'iNik Dev.';
+
+// Theme URL
+$url = 'https://inik.pw';
+
+// Theme Version
+$version = '1.0.m';
+?>
